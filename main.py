10b719ccd43510bd66add6c883a4d69db6a0f424
The Myth <thenoobdude21.12@gmail.com>
2021-01-13 22:09:41 +0000

    Changed colors

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import discord
 import os
 import asyncio
-import random
 from discord.ext import commands
 from keep_alive import keep_alive
 
@@ -16,10 +15,10 @@ async def on_message(message):
     if "<@&791756207618260992>" in message.content:
         role = discord.utils.get(message.guild.roles, id=791756207618260992)
         await role.edit(mentionable=False)
-        await message.channel.send(embed=discord.Embed(color=random.randint(0, 0xffffff), description=":lock: The help role is now locked for 5 minutes."))
+        await message.channel.send(embed=discord.Embed(color=discord.Color.red(), description=":lock: The help role is now locked for 10 minutes."))
         await asyncio.sleep(600)
         await role.edit(mentionable=True)
-        await message.channel.send(embed=discord.Embed(color=random.randint(0, 0xffffff), description=":unlock: The help role is now unlocked."))
+        await message.channel.send(embed=discord.Embed(color=discord.Color.green(), description=":unlock: The help role is now unlocked."))
 
 keep_alive()
 bot.run(os.environ.get('BOT_TOKEN'))
